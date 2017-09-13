3aa425e2acb9ecc97f7915b25874411c497eb921
slayercoder <ms81291@gmail.com>
2017-09-14 01:23:56 +0530

    schema and db test

---
+++ models/schemas/feedSchema.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+var feedSchema = new Schema({
+    title : {type : String, required : true },
+    description : {type : String, required : true},
+    date : {type : Date},
+    link : {type : String, required : true},
+    creator : {type : String, required : true},
+    // media_url : {type : String, required : true},
+    category : {type : String, required : true}
+});
+
+
+var Feed = mongoose.model("Feed", feedSchema);
+
+module.exports = Feed;
--- server.js
+++ server.js
@@ -1,18 +1,47 @@
 const express = require("express");
 const app = express();
-const morgan = require("morgan");
-const async = require("async");
 const path = require("path");
 const config = require('./models/config.js');
 const port = process.env.PORT || config.port;
+
+// third party modules
+const morgan = require("morgan");
+const async = require("async");
+const parser = require("rss-parser");
+
+// mongoDB methods wrapper
+const mongoose = require("mongoose");
+const Feed = require("./models/schemas/feedSchema");
+const mongodb = require("mongodb");
+mongoose.connect("mongodb://localhost/appDb");
+
+// application middlewares
 app.use(morgan("dev"));
 
 
+setInterval(function(){
+    
+        parser.parseURL("http://www.toptal.com/blog.rss", function(err,parsed){
+            
+            
+                for(var i = 0; i < len - documentCount; i++){
+                    var entry = new Feed({
+                        title : item.title,
+                        description : item.content,
+                        date : item.pubDate,
+                        link : item.link,
+                        creator : item.creator,
+                        category : "nodejs"
+                    });
+                    entry.save(function(err){
+                        if(err) throw err;
+                        console.log("feed added");
+                    });                 
+                }
+            });
 
-app.get("/", function(req,res){
-    res.json({"key" : "value"});
+},172800000);
 
-});
 
 
 
