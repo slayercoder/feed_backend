a12f83077efb6eaf83b152592045f34f35cddd2e
Suraj Prakash <surajprakash094@gmail.com>
2017-09-16 13:02:05 +0530

    cross origin request handling

--- server.js
+++ server.js
@@ -28,6 +28,12 @@ db.once("open", function(){
 
 // application middlewares
 app.use(morgan("dev"));
+app.use(function(req,res,next){
+    res.header('Access-Control-Allow-Origin','*');
+    res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers','Content-Type');
+    next();
+})
 
 app.get("/", function(req,res){
     
